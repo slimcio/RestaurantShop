e34f2d46e7cc225f49258fac04b3a604bbae92f9
slimcio <87947547+slimcio@users.noreply.github.com>
2021-07-31 00:39:39 +0200

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 600) {
+	if (scroll_position > 200) {
 		header.style.backgroundColor = '#70a4ff';
 	} else {
 		header.style.backgroundColor = 'transparent';
